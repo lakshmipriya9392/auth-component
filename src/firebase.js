5669dc4874e8324a06fd6b2942994f9cab34793b
lakshmipriya9392 <66982780+lakshmipriya9392@users.noreply.github.com>
2021-06-24 19:35:54 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
+  // Your web app's Firebase configuration
+  const app = firebase.initializeApp({
+    apiKey: "AIzaSyAjgBKo9qU-fB1mCJ117GQEhGt3iDm0zsg",
+    authDomain: "auth-component-91e78.firebaseapp.com",
+    projectId: "auth-component-91e78",
+    storageBucket: "auth-component-91e78.appspot.com",
+    messagingSenderId: "86132999792",
+    appId: "1:86132999792:web:e32f2c3a86223ee05cd6b5"
+  });
+  
+
+  export default app;
